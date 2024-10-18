0958571339d2b6c3a261baef6896ccbee26de9fc
liuwenqing <610335123@qq.com>
2024-10-18 18:08:49 +0800

    commit

--- zipkin-storage/clickhouse/src/main/java/zipkin2/clickhouse/Constants.java
+++ zipkin-storage/clickhouse/src/main/java/zipkin2/clickhouse/Constants.java
@@ -38,7 +38,7 @@ public class Constants {
     + "where time >= ? AND time <= ? AND remoteEndpointServiceName NOT LIKE '%:%' AND localEndpointServiceName NOT LIKE '%:%' "
     + "group by kind,localEndpointServiceName,remoteEndpointServiceName";
 
-  public static final String INSERT_SQL = "INSERT INTO %s SETTINGS async_insert=1, wait_for_async_insert=1 (traceId,parentId,id,kind,duration,name,timestamp,status,timestampMillis,localEndpointIpv4,"
+  public static final String INSERT_SQL = "INSERT INTO %s (traceId,parentId,id,kind,duration,name,timestamp,status,timestampMillis,localEndpointIpv4,"
     + "localEndpointServiceName,localEndpointPort,remoteEndpointIpv4,remoteEndpointServiceName,remoteEndpointPort,tagsRpcMethod,tagsRpcService,tagsAppname,"
     + "tagsComponent,tagsHttpUrl,tagsHttpMethod,tagsCatMessageId,tagsHttpPath,tagsHttpRequestSize,tagsHttpResponseSize,tagsHttpStatusCode,tagsLocalIpv4,"
     + "tagsPeerIpv4,tagsPeerService,tagsPeerPort,tagsPeerHostname,tagsPeerIpv6,tagsProcessId,tagsSpanKind,tagsWorkerId,logFilePath,time,tagsSql) VALUES ";
